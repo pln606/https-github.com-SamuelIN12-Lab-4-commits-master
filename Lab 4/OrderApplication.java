919c16fd54a91061a2e8d1a6bde3cd8d55f2fa1c
Ranil Fernando <ranilmf@gmail.com>
2017-10-25 15:22:27 -0600

    Created classes, add base functions and vars from class diagram

---
+++ Lab 4/OrderApplication.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class OrderApplication {
+
+	
+	boolean createOrder(Order a) {
+		return false;
+	}
+	
+	boolean importCatalogue() {
+		return false;
+	}
+	
+}
+
+
---
+++ Lab 4/OrderLine.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class OrderLine {
+
+	int quantity;
+	double price;
+	
+	OrderLine(Product product, int quantity, double price) {
+		
+	}
+	
+	Product getProduct() {
+		return null;
+	}
+	
+	int getQuantity() {
+		return quantity;
+	}
+	
+	double getPrice() {
+		return price;
+	}
+	
+}
